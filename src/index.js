70946d2b73e81b1bceb737182e59286f986df884
Mykola <nazimkoff@gmail.com>
2020-10-12 19:40:04 +0300

    feat: done all

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let result, firstDigit, secondDigit, thirdDigit;
+
+    firstDigit = Math.trunc(Math.abs(n)/100);
+    secondDigit = Math.trunc((Math.abs(n)%100)/10);
+    thirdDigit = Math.trunc (Math.abs(n)%10);
+    result = String(thirdDigit) + String(secondDigit) + String(firstDigit);
+    return result;
 }
